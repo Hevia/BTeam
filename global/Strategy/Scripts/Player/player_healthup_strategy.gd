2be192dabe3507a6dbc115f6c64bcd2ffd3e4e73
lucidinceptor <stephgamedevving@gmail.com>
2025-04-07 03:41:50 -0400

    Added Hitbox to primary
    Added hitbox to primary and rearranged nodes in player tree

--- global/Strategy/Scripts/Player/player_healthup_strategy.gd
+++ global/Strategy/Scripts/Player/player_healthup_strategy.gd
@@ -2,7 +2,7 @@ class_name PlayerHealthupStrategy
 extends BaseUpgradeStrategy
 
 func apply_player_upgrade(player: Player):
-	var health_handler: Health = player.get_child(2)
+	var health_handler: Health = player.get_child(1)
 	health_handler.max_health += 2
 	health_handler.health += 2
 	print("Max Health: ", health_handler.max_health)
